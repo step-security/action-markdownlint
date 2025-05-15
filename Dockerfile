4bad9f666ce672bc8c6cd99d6d5435f7870130c7
amanstep <aman@stepsecurity.io>
2025-05-16 01:26:37 +0530

    feat: Initial release

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:20-bullseye-slim
+
+ENV MARKDOWNLINT_CLI_VERSION=v0.41.0
+
+RUN npm install -g "markdownlint-cli@$MARKDOWNLINT_CLI_VERSION"
+
+ENV REVIEWDOG_VERSION=v0.20.2
+
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        ca-certificates \
+        git \
+        wget \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/fd59714416d6d9a1c0692d872e38e7f8448df4fc/install.sh \
+    | sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+CMD []
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+cd "${GITHUB_WORKSPACE}" || exit 1
+git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
+
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+# shellcheck disable=SC2086
+markdownlint ${INPUT_MARKDOWNLINT_FLAGS:-.} 2>&1 \
+  | reviewdog \
+      -efm="%f:%l:%c %m" \
+      -efm="%f:%l %m" \
+      -name="markdownlint" \
+      -reporter="${INPUT_REPORTER:-github-pr-check}" \
+      -filter-mode="${INPUT_FILTER_MODE}" \
+      -fail-level="${INPUT_FAIL_LEVEL}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -level="${INPUT_LEVEL}" \
+      ${INPUT_REVIEWDOG_FLAGS} || EXIT_CODE=$?
+
+ # github-pr-review only diff adding
+if [ "${INPUT_REPORTER}" = "github-pr-review" ]; then
+  # fix
+  markdownlint --fix ${INPUT_MARKDOWNLINT_FLAGS:-.} 2>&1 || true
+
+  TMPFILE=$(mktemp)
+  git diff > "${TMPFILE}"
+
+  git stash -u
+
+  # shellcheck disable=SC2086
+  reviewdog                        \
+    -f=diff                        \
+    -f.diff.strip=1                \
+    -name="markdownlint-fix"       \
+    -reporter="github-pr-review"   \
+    -filter-mode="diff_context"    \
+    -level="${INPUT_LEVEL}"        \
+    ${INPUT_REVIEWDOG_FLAGS} < "${TMPFILE}"
+
+  git stash drop || true
+fi
+
+exit ${EXIT_CODE}
